ed57f578b492e1e303b627e774f5f56e65fff877
chewbacca1991 <mounafbanbo@hotmail.com>
2025-08-31 12:28:03 +0330

    Add comments in English for clarity

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 # This script is the entry point to run the Flask application.
 from app import app
 
+# Check if this script is being run directly
 if __name__ == '__main__':
+    # Run the Flask application in debug mode
     app.run(debug=True)
